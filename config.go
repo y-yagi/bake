2ec4130ef45fd0e2f48b1501eee742355ef654c8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-06-06 14:45:33 +0900

    Allow to specify `stdout` and `stderr`

--- config.go
+++ config.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"io"
+	"os"
 	"runtime"
 
 	"github.com/BurntSushi/toml"
@@ -11,9 +13,11 @@ import (
 
 // Command represents a command to run.
 type Command struct {
-	name string
-	args []string
-	envs []string
+	name   string
+	args   []string
+	envs   []string
+	stdout io.Writer
+	stderr io.Writer
 }
 
 // BakeFileVariable represents a variables of configuration file.
@@ -96,19 +100,52 @@ func (c *Config) BuildCommands(target string) ([]Command, error) {
 		definedTasks[dependency] = true
 
 		if len(t.Command) > 0 {
-			commands = append(commands, Command{name: t.Command[0], args: t.Command[1:], envs: t.Environments})
+			stdout, stderr, err := c.openStdoutAndStderr(t.Stdout, t.Stderr)
+			if err != nil {
+				return nil, err
+			}
+			commands = append(commands, Command{name: t.Command[0], args: t.Command[1:], envs: t.Environments, stdout: stdout, stderr: stderr})
 		}
 	}
 
 	if len(task.Command) > 0 {
-		commands = append(commands, Command{name: task.Command[0], args: task.Command[1:], envs: task.Environments})
+		stdout, stderr, err := c.openStdoutAndStderr(task.Stdout, task.Stderr)
+		if err != nil {
+			return nil, err
+		}
+		commands = append(commands, Command{name: task.Command[0], args: task.Command[1:], envs: task.Environments, stdout: stdout, stderr: stderr})
 	}
 
 	if len(task.Commands) > 0 {
-		for _, c := range task.Commands {
-			commands = append(commands, Command{name: c[0], args: c[1:], envs: task.Environments})
+		for _, cmd := range task.Commands {
+			stdout, stderr, err := c.openStdoutAndStderr(task.Stdout, task.Stderr)
+			if err != nil {
+				return nil, err
+			}
+			commands = append(commands, Command{name: cmd[0], args: cmd[1:], envs: task.Environments, stdout: stdout, stderr: stderr})
 		}
 	}
 
 	return commands, nil
+}
+
+func (c *Config) openStdoutAndStderr(stdout string, stderr string) (io.Writer, io.Writer, error) {
+	stdoutWriter, stderrWriter := io.Discard, io.Discard
+	var err error
+
+	if len(stdout) != 0 {
+		stdoutWriter, err = os.OpenFile(stdout, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to open 'stdout' path: %v, %v", stdout, err)
+		}
+	}
+
+	if len(stderr) != 0 {
+		stderrWriter, err = os.OpenFile(stderr, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to open 'stderr' path: %v, %v", stderr, err)
+		}
+	}
+
+	return stdoutWriter, stderrWriter, nil
 }
